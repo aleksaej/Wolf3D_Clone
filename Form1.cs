56935e5deeb5df96571bc8af1b71c80a5b3c7669
aleksaej <all.ukolov@gmail.com>
2025-06-18 00:33:58 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3DTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+
+            Program.game = new Game();
+            Program.game.Init();
+
+            Thread thr = new Thread(Draw);
+            thr.Start();
+
+        }
+
+        bool released = false;
+
+        public void Draw()
+        {
+
+
+            while (!this.Visible) ;
+            while (this.Visible)
+            {
+                Bitmap display = new Bitmap(320, 200);
+                Program.game.Graphics = Graphics.FromImage(display);
+                Program.game.Update();
+                Program.game.Draw();
+                pictureBox1.Image = display;
+            }
+        }
+
+        public delegate void _setPictureBox(Bitmap bmp);
+        public void SetPictureBox(Bitmap bmp)
+        {
+            if (pictureBox1.InvokeRequired)
+            {
+                try
+                {
+                    pictureBox1.Invoke(new _setPictureBox(SetPictureBox), bmp);
+                }
+                catch
+                {
+
+                }
+            }
+            else pictureBox1.Image = bmp;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!Program.game.Pressedkeys.Contains(e.KeyCode))
+                Program.game.Pressedkeys.Add(e.KeyCode);
+            if (e.Modifiers == Keys.Alt)
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    SwitchFullScreen();
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Program.game.Pressedkeys.Contains(e.KeyCode))
+                Program.game.Pressedkeys.Remove(e.KeyCode);
+        }
+
+        bool fullscreen = false;
+
+        public void SwitchFullScreen()
+        {
+            fullscreen = !fullscreen;
+            if (fullscreen)
+            {
+                originalBounds = this.Bounds;
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.Focus();
+                this.BringToFront();
+                this.TopMost = true;
+                this.Bounds = Screen.PrimaryScreen.Bounds;
+            }
+            else
+            {
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                this.WindowState = FormWindowState.Normal;
+                this.TopMost = false;
+                this.Bounds = originalBounds;
+            }
+        }
+
+        public Rectangle originalBounds;
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0112)
+            {
+                if (m.WParam == new IntPtr(0xF030))
+                {
+                    if (!fullscreen) SwitchFullScreen();
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Audio song = new Audio("Sounds/Level1.wav", true);
+            song.Play();
+        }
+    }
+}
---
+++ Maths.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DTest
+{
+    public class Angle
+    {
+        private double _actualValue = 0;
+        private double Value
+        {
+            get { return _actualValue; }
+            set
+            {
+                _actualValue = value;
+                while (_actualValue < 0) _actualValue += 360;
+                _actualValue %= 360;
+            }
+        }
+        public Angle(double angle)
+        {
+            Value = angle;
+        }
+
+
+        public double GetRad()
+        {
+            return Value * (Math.PI / 180);
+        }
+
+        public static double GetRad(Angle angle)
+        {
+            return angle.GetRad();
+        }
+
+        public void SetRad(double value)
+        {
+            Value = value / (Math.PI / 180);
+        }
+
+        public static implicit operator Angle(double d)
+        {
+            return new Angle(d);
+        }
+
+        public static implicit operator double(Angle a)
+        {
+            return a.Value;
+        }
+
+        public override string ToString()
+        {
+            return (Value).ToString();
+        }
+    }
+
+    public class Vector2
+    {
+        public double X;
+        public double Y;
+        public Vector2(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Dist(Vector2 vector)
+        {
+            return Vector2.Dist(this, vector);
+        }
+
+        public static double Dist(Vector2 vector1, Vector2 vector2)
+        {
+            double width = Math.Abs(vector1.X - vector2.X);
+            double height = Math.Abs(vector1.Y - vector2.Y);
+            return Math.Sqrt(width * width + height * height);
+        }
+
+        public bool Equals(Vector2 vector)
+        {
+            return ((X == vector.X) && (Y == vector.Y));
+        }
+
+        public override bool Equals(object vector)
+        {
+            if (vector.GetType() == typeof(Vector2))
+                return Equals((Vector2)vector);
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return X.GetHashCode() ^ Y.GetHashCode();
+        }
+
+        public static bool operator == ( Vector2 vector, Vector2 vector2)
+        {
+            return vector2.Equals(vector);
+        }
+
+        public static bool operator != (Vector2 vector, Vector2 vector2)
+        {
+            return !vector2.Equals(vector);
+        }
+    }
+
+    public class Rectangle
+    {
+        public double X;
+        public double Y;
+        public double Width;
+        public double Height;
+
+        public Rectangle(double X, double Y, double Width, double Height)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+        public Rectangle(Vector2 Position, Vector2 Size)
+        {
+            X = Position.X;
+            Y = Position.Y;
+            Width = Size.X;
+            Height = Size.Y;
+        }
+
+        public Rectangle(Vector2 Position, double Width, double Height)
+        {
+            X = Position.X;
+            Y = Position.Y;
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+        public Rectangle(double X, double Y, Vector2 Size)
+        {
+            this.X = X;
+            this.Y = Y;
+            Width = Size.X;
+            Height = Size.Y;
+        }
+
+        public bool Intersects(Rectangle rectangle2)
+        {
+            return ((X < rectangle2.X + rectangle2.Width) && (X + Width > rectangle2.X) &&
+                    (Y < rectangle2.Y + rectangle2.Height) && (Y + Height > rectangle2.Y));
+        }
+        public bool Intersects(Vector2 vector)
+        {
+            return ((X < vector.X) && (X + Width > vector.X) &&
+                    (Y < vector.Y) && (Y + Height > vector.Y));
+        }
+
+        public static bool Intersects(Rectangle rectangle, Rectangle rectangle2)
+        {
+            return rectangle.Intersects(rectangle2);
+        }
+
+        public static bool Intersects(Rectangle rectangle, Vector2 vector)
+        {
+            return rectangle.Intersects(vector);
+        }
+
+        public static implicit operator Rectangle (System.Drawing.Rectangle rectangle)
+        {
+            return new Rectangle(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height);
+        }
+
+        public static implicit operator Rectangle(System.Drawing.RectangleF rectangle)
+        {
+            return new Rectangle(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height);
+        }
+
+        public static implicit operator System.Drawing.Rectangle(Rectangle rectangle)
+        {
+            return new System.Drawing.Rectangle((int)rectangle.X, (int)rectangle.Y, (int)rectangle.Width, (int)rectangle.Height);
+        }
+
+        public static implicit operator System.Drawing.RectangleF(Rectangle rectangle)
+        {
+            return new System.Drawing.RectangleF((float)rectangle.X, (float)rectangle.Y, (float)rectangle.Width, (float)rectangle.Height);
+        }
+
+        public override string ToString()
+        {
+            return $"X: {X}; Y: {Y}; Width: {Width}; Height: {Height}";
+        }
+    }
+}
